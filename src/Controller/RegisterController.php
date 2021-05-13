7dbc58104e1e71a8b8bfb9118fdbf3f5b246ccde
Moi Moi <ericfreelance@gmail.com>
2021-05-13 20:50:18 +0200

    Mise en place du formulaire d'inscription, sauvegarde infos user, encodage mdp, validation des données et translation fr

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,30 +2,47 @@
 
 namespace App\Controller;
 
-use App\Entity\User; // la class User() est importée
-use App\Form\RegisterType; // la class RegisterType() est importée
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
-        // on instancie la classe User et on l'affecte à la variable user
         $user = new User();
 
-        // on instancie le formulaire avec la méthode createForm et on l'affecte à la variable $form
-        // la méthode demande deux paramètres : la classe du formulaire et les données ici notre objet $user
         $form = $this->createForm(RegisterType::class, $user);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
         return $this->render('register/index.html.twig', [
-            // on passe le formulaire en variable à notre template
-            // une clé 'form' (vous pouvez mettre autre chose)
-            // puis on demande la création de la vue de notre objet $form avec la méthode createView()
             'form' => $form->createView()
         ]);
     }
